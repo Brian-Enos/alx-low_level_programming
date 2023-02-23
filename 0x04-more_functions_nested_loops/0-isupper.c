2cb14bfae72ef96189116c2acd3e215d6ebeabee
Brian-Enos <brianenos9199@gmail.com>
2023-02-23 05:43:38 -0800

    main.h

--- 0x04-more_functions_nested_loops/0-isupper.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-{
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
-}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,3 +24,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+#endif
