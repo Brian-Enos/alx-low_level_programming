54c3e1e83dd4e964e276a5d016368c6cb2e687e0
Brian-Enos <brianenos9199@gmail.com>
2023-02-23 05:55:07 -0800

    A function that checks for uppercase character

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -24,4 +22,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-#endif
