02f0c296972013426b469c28d51e38abd8194123
Brian-Enos <brianenos9199@gmail.com>
2023-02-23 05:11:32 -0800

    A function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
