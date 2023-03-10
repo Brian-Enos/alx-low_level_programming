c952030671573c8c6c66ae06e15498f1e892b0a7
Brian-Enos <brianenos9199@gmail.com>
2023-03-10 03:46:54 -0800

    A A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/mylib/mylib.c
@@ -0,0 +1,91 @@
+#include "mylib.h"
+
+int _putchar(char c)
+{
+    // Your implementation here
+}
+
+int _islower(int c)
+{
+    // Your implementation here
+}
+
+int _isalpha(int c)
+{
+    // Your implementation here
+}
+
+int _abs(int n)
+{
+    // Your implementation here
+}
+
+int _isupper(int c)
+{
+    // Your implementation here
+}
+
+int _isdigit(int c)
+{
+    // Your implementation here
+}
+
+int _strlen(char *s)
+{
+    // Your implementation here
+}
+
+void _puts(char *s)
+{
+    // Your implementation here
+}
+
+char *_strcpy(char *dest, char *src)
+{
+    // Your implementation here
+}
+
+int _atoi(char *s)
+{
+    // Your implementation here
+}
+
+char *_strcat(char *dest, char *src)
+{
+    // Your implementation here
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+    // Your implementation here
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    // Your implementation here
+}
+
+int _strcmp(char *s1, char *s2)
+{
+    // Your implementation here
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    // Your implementation here
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    // Your implementation here
+}
+
+char *_strchr(char *s, char c)
+{
+    // Your implementation here
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+    // Your implementation here
+
