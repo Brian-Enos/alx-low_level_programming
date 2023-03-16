87372b46907293caa087ce94b0ceaaf13d639f3f
Brian-Enos <brianenos9199@gmail.com>
2023-03-16 06:53:18 -0700

    A program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,34 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 1 on error
  */
-
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int num1, num2;
+    unsigned long mul;
+    int num1, num2;
 
-	if (argc != 3)
-	{ printf("Error\n");
-		exit(98); }
+    /* Verify that the program was called with two arguments */
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (1);
+    }
 
-	for (num1 = 1; num1 < argc; num1++)
-	{
+    /* Verify that both arguments are valid positive integers */
+    for (num1 = 1; num1 < argc; num1++)
+    {
+        for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+        {
+            if (argv[num1][num2] < '0' || argv[num1][num2] > '9')
+            {
+                printf("Error\n");
+                return (1);
+            }
+        }
+    }
 
-		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
-		{
+    /* Perform the multiplication using strtoul */
+    mul = strtoul(argv[1], NULL, 10) * strtoul(argv[2], NULL, 10);
 
-			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
-			{  printf("Error\n");
-				exit(98); }
-		}
-	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
+    printf("%lu\n", mul);
+    return (0);
 }
+
