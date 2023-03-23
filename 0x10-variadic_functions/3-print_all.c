6ae8251294e2050bcae6140b746ae09715e14357
Brian-Enos <brianenos9199@gmail.com>
2023-03-23 07:52:36 -0700

    A function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,9 +9,9 @@ void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
 /**
- * print_char - prints a char
- * @arg: a list of arguments
- * Return: return 0
+ * print_char - Prints a char.
+ * @arg: A list of arguments pointing to
+ *       the character to be printed.
  */
 void print_char(va_list arg)
 {
@@ -23,8 +23,8 @@ void print_char(va_list arg)
 
 /**
  * print_int - Prints an int.
- * @arg: A list of arguments
- * Return: return 0
+ * @arg: A list of arguments pointing to
+ *       the integer to be printed.
  */
 void print_int(va_list arg)
 {
@@ -36,8 +36,8 @@ void print_int(va_list arg)
 
 /**
  * print_float - Prints a float.
- * @arg: A list of arguments
- * Return: return 0
+ * @arg: A list of arguments pointing to
+ *       the float to be printed.
  */
 void print_float(va_list arg)
 {
@@ -49,8 +49,8 @@ void print_float(va_list arg)
 
 /**
  * print_string - Prints a string.
- * @arg: A list of arguments pointing to string
- * Return: return 0
+ * @arg: A list of arguments pointing to
+ *       the string to be printed.
  */
 void print_string(va_list arg)
 {
@@ -69,9 +69,12 @@ void print_string(va_list arg)
 
 /**
  * print_all - Prints anything, followed by a new line.
- * @format: A string of characters representing the argument
+ * @format: A string of characters representing the argument types.
  * @...: A variable number of arguments to be printed.
- * Description: argument selection
+ *
+ * Description: Any argument not of type char, int, float,
+ *              or char * is ignored.
+ *              If a string argument is NULL, (nil) is printed instead.
  */
 void print_all(const char * const format, ...)
 {
