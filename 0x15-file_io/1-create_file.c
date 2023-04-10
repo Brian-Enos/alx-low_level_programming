3bd590c6631bd7a6dd12adb950455b5cd3a0f439
Brian-Enos <brianenos9199@gmail.com>
2023-04-10 08:41:10 -0700

    A  function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,23 @@
+#include "main.h"
 
+int create_file(const char *filename, char *text_content)
+{
+    int o, w, len = 0;
+
+    if (!filename)
+        return (-1);
+
+    if (text_content)
+        for (len = 0; text_content[len]; len++)
+            ;
+
+    o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+    w = write(o, text_content, len);
+
+    if (o == -1 || w == -1)
+        return (-1);
+
+    close(o);
+
+    return (1);
+}
