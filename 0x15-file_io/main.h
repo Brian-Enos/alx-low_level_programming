1ebb59e26e7793d47013a7450b09d184ec8ceabd
Brian-Enos <brianenos9199@gmail.com>
2023-04-10 08:14:17 -0700

    pushing readme and main.h files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+fndef MAIN_H
+#define MAIN_Hi
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
